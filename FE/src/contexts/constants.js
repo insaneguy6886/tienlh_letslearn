c14d3b0550ea319c30b1eccdf2cac7b3490640d8
tienlehung1080 <tienlehung1080@gmail.com>
2023-08-08 18:34:18 +0700

    Done

--- FE/src/contexts/constants.js
+++ FE/src/contexts/constants.js
@@ -1,7 +1,7 @@
 export const apiUrl =
   process.env.NODE_ENV !== "production"
     ? "http://localhost:5000/api"
-    : "somedeployURL";
+    : "https://dry-journey-30547-28c42f7c0359.herokuapp.com/api";
 
 export const LOCAL_STORAGE_TOKEN_NAME = "learnit-mern";
 

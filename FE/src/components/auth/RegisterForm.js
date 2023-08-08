1caae72f32f6ff09e2e574935f3487dd54490d83
tienlehung1080 <tienlehung1080@gmail.com>
2023-08-08 13:42:43 +0700

    Done

---
+++ FE/src/components/auth/RegisterForm.js
@@ -0,0 +1,107 @@
+import Button from "react-bootstrap/Button";
+import Form from "react-bootstrap/Form";
+import { Link, Navigate } from "react-router-dom";
+import { useContext, useState } from "react";
+import { AuthContext } from "../../contexts/AuthContext";
+import AlertMessage from "../layout/AlertMessage";
+
+const RegisterForm = () => {
+  // Context
+  const { registerUser } = useContext(AuthContext);
+
+  // Local state
+  const [registerForm, setRegisterForm] = useState({
+    username: "",
+    password: "",
+    confirmPassword: "",
+  });
+
+  const [alert, setAlert] = useState(null);
+
+  const { username, password, confirmPassword } = registerForm;
+
+  const onChangeRegisterForm = (event) =>
+    setRegisterForm({
+      ...registerForm,
+      [event.target.name]: event.target.value,
+    });
+
+  const register = async (event) => {
+    event.preventDefault();
+
+    if (password !== confirmPassword) {
+      setAlert({ type: "danger", message: "Passwords do not match" });
+      setTimeout(() => setAlert(null), 5000);
+      return;
+    }
+
+    try {
+      const registerData = await registerUser(registerForm);
+      if (registerData.success) {
+        // Show success notification
+        setAlert({ type: "success", message: "Registration successful!" });
+        setTimeout(() => setAlert(null), 5000);
+        // Navigate to login page after successful registration
+        return <Navigate to="/login" />;
+      } else {
+        setAlert({ type: "danger", message: registerData.message });
+        setTimeout(() => setAlert(null), 5000);
+      }
+    } catch (error) {
+      console.log(error);
+    }
+  };
+
+  return (
+    <>
+      <Form className="my-4" onSubmit={register}>
+        <AlertMessage info={alert} />
+
+        <Form.Group>
+          <Form.Control
+            type="text"
+            placeholder="Username"
+            name="username"
+            required
+            value={username}
+            onChange={onChangeRegisterForm}
+          />
+        </Form.Group>
+        <Form.Group className="my-3">
+          <Form.Control
+            type="password"
+            placeholder="Password"
+            name="password"
+            required
+            value={password}
+            onChange={onChangeRegisterForm}
+          />
+        </Form.Group>
+        <Form.Group>
+          <Form.Control
+            type="password"
+            placeholder="Confirm Password"
+            name="confirmPassword"
+            required
+            value={confirmPassword}
+            onChange={onChangeRegisterForm}
+          />
+        </Form.Group>
+
+        <Button variant="success" type="submit" className="mt-3">
+          Register
+        </Button>
+      </Form>
+      <p>
+        Already have an account?
+        <Link to="/login">
+          <Button variant="info" size="sm" className="ml-2">
+            Login
+          </Button>
+        </Link>
+      </p>
+    </>
+  );
+};
+
+export default RegisterForm;
